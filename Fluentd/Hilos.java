7b21fb3a509944d9f8820d96b078218f773994be
albertoblaz <hola@albertoblazquez.net>
2012-08-20 10:15:08 +0200

    Testing Fluentd

---
+++ Fluentd/Hilos.java
@@ -0,0 +1,59 @@
+package Prueba;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.fluentd.logger.FluentLogger;
+
+
+public class Hilos extends Thread {
+
+	/* Attributes */
+
+	private int i;
+
+	private static FluentLogger LOG = FluentLogger.getLogger("app", "192.168.1.64", 24224);
+
+
+	/* Constructor */
+	
+	public Hilos(int n) {
+		this.i = n;
+	}
+
+	
+	/* Methods */
+
+	public void run(){
+		if (i%2 == 0)
+			testa();
+		else
+			testb();
+		return;
+	}
+
+	public void testa() {
+		for (int i = 0; i < 100; i++) {
+			Map<String, Object> data = new HashMap<String, Object>();
+			data.put("from", "Hilo " + this.i);
+			data.put("to", "Myself");
+			data.put("Valor", i);
+			LOG.log("prueba", data);
+		}
+	}
+
+	public void testb() {
+		for (int i = 0; i < 150; i++) {
+			Map<String, Object> data = new HashMap<String, Object>();
+			data.put("from", "Hilo " + this.i);
+			data.put("to", "Myself");
+			data.put("Valor", i+10000);
+			LOG.log("prueba", data);
+		}
+	}
+	
+	public static void cerrar(){
+		LOG.close();
+	}
+
+}
---
+++ Fluentd/Main.java
@@ -0,0 +1,78 @@
+package Prueba;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.fluentd.logger.FluentLogger;
+
+public class Main {
+
+	/* Attributes */
+
+	private FluentLogger LOG = FluentLogger.getLogger("app","192.168.1.64", 24224);
+
+
+	/* Methods */
+
+	public void doApplicationLogic() {
+		Map<String, Object> data = new HashMap<String, Object>();
+		data.put("from", "userA");
+		data.put("to", "userB");
+		LOG.log("prueba", data);
+	}
+
+	public void test1(){
+		for (int i = 0; i < 1000; i++) {
+			Map<String, Object> data = new HashMap<String, Object>();
+			data.put("from", "Me");
+			data.put("to", "Myself");
+			data.put("Valor", i);
+			LOG.log("prueba", data);
+		}
+	}
+
+	public void test2(){
+		Hilos[] h=new Hilos[2];
+		h[0]=new Hilos(1);
+		h[1]=new Hilos(2);
+		h[0].start();
+		h[1].start();
+		try {
+			h[0].join();
+			h[1].join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void test3(){
+		Hilos h[]=new Hilos [1000];
+		for (int i = 0; i < h.length; i++) {
+			h[i]=new Hilos(i);
+			h[i].start();
+		}
+		for (int i = 0; i < h.length; i++) {
+			try {
+				h[i].join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+
+	/* Main */
+
+	public static void main(String[] args) {
+		Ppal p = new Ppal();
+		p.doApplicationLogic();
+		p.test1();
+		p.LOG.close();
+		p.test2();
+		p.test3();
+		Hilos.cerrar();
+	}
+
+}
